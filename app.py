c55449cc3c54032579ec46b8c513b55d076bb187
Marcin <marcin919@op.pl>
2018-04-25 19:05:28 +0200

    a

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
+#DATABASE_URL = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+#db = SQLAlchemy(app)
 
 
 @app.route('/')
