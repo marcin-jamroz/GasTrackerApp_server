b036a1eed87711b4be58d2e824d4cc088a33a72f
Marcin <marcin919@op.pl>
2018-04-25 19:33:21 +0200

    b

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return 'KAAAAAPPPPPPAAA'
+    result = db.engine.execute("SELECT network_name FROM gas_station WHERE station_id=237")
+    print(type(result))
+    print(result)
+    return result, type(result), result.dict
 
 
 if __name__ == '__main__':
