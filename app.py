9b8784c12ec1ce44b12708d4603189bfb386b08f
Marcin <marcin919@op.pl>
2018-04-25 19:58:38 +0200

    b

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     result = dict(result)
     print('----------->', type(result))
     print('----------->', result)
-    return ''.join(result.popitem(), ' : '), type(result)
+    return ' : '.join(result.popitem()), type(result)
 
 
 if __name__ == '__main__':
