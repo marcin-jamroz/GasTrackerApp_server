f0f89d10aa06c36a9fecd4056b76f64e858937c0
Filip Rak <filiprak@wp.pl>
2018-05-22 17:08:19 +0200

    Add error handling

--- app.py
+++ app.py
@@ -29,14 +29,20 @@ def get_station(id):
 @app.route('/networks', defaults={'id': None})
 @app.route('/networks/<id>')
 def get_network(id):
-    if (id):
-        query = '''SELECT * FROM gas_networks WHERE network_id=:id'''
-        result = db.engine.execute(text(query), {"id" : id}).fetchone()
-        return jsonify(dict(result)), 200
-    else:
-        query = '''SELECT * FROM gas_networks'''
-        result = db.engine.execute(text(query))
-        return jsonify([dict(r) for r in result]), 200
+    try:
+        if (id):
+            query = '''SELECT * FROM gas_networks WHERE network_id=:id'''
+            result = db.engine.execute(text(query), {"id" : id}).fetchone()
+            return jsonify(dict(result)), 200
+        else:
+            query = '''SELECT * FROM gas_networks'''
+            result = db.engine.execute(text(query))
+            return jsonify([dict(r) for r in result]), 200
+    except Exception as e:
+        result = dict()
+        result['type'] = type(e).__name__
+        result['message'] = e.message if hasattr(e, 'message') else str(e)
+        return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
